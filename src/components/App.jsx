7e81a95bcc3e96c680fd616e5b7a5b89a62da2d6
Phu <phugnguyen@gmail.com>
2019-07-11 14:07:52 -0700

    fetch physicians list from backend in component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import PhysicianList from "./PhysicianListComponent";
+import AppointmentList from "./AppointmentListComponent";
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+        physicians = [],
+    };
   }
+
+  componentDidMount() {
+      fetch("/api/physicians")
+      .then(physicians => physicians.json())
+      .then(physicians => (
+          this.setState({ physicians: physicians})
+      ).catch( err => console.log(err));
+  }
+
+  
 
   render() {
     return (
