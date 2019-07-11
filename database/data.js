8bdc18bd10f5fefd164f665af6397c18155fd6fa
Phu <phugnguyen@gmail.com>
2019-07-11 14:52:34 -0700

    render appointments

--- database/data.js
+++ database/data.js
@@ -6,19 +6,19 @@ const appointmentList = {
     lastName: "Hibbert",
     patients: [
       {
-        num: 1,
+        number: 1,
         name: "Malory Archer",
         time: "8:00AM",
         kind: "Follow-up"
       },
       {
-        num: 2,
+        number: 2,
         name: "Barry Dillon",
         time: "9:00AM",
         kind: "New Patient"
       },
       {
-        num: 3,
+        number: 3,
         name: "Woodhouse",
         time: "10:00AM",
         kind: "Follow-up"
@@ -31,31 +31,31 @@ const appointmentList = {
     lastName: "Krieger",
     patients: [
       {
-        num: 1,
+        number: 1,
         name: "Sterling Archer",
         time: "8:00AM",
         kind: "New Patient"
       },
       {
-        num: 2,
+        number: 2,
         name: "Cyril Figis",
         time: "8:30AM",
         kind: "Follow-up"
       },
       {
-        num: 3,
+        number: 3,
         name: "Ray Gilette",
         time: "9:00AM",
         kind: "Follow-up"
       },
       {
-        num: 4,
+        number: 4,
         name: "Lana Kane",
         time: "9:30AM",
         kind: "New Patient"
       },
       {
-        num: 5,
+        number: 5,
         name: "Pam Poovey",
         time: "10:00AM",
         kind: "New Patient"
@@ -68,31 +68,31 @@ const appointmentList = {
     lastName: "Riviera",
     patients: [
       {
-        num: 1,
+        number: 1,
         name: "Cheryl Tunt",
         time: "8:00AM",
         kind: "Follow-up"
       },
       {
-        num: 2,
+        number: 2,
         name: "Len Drexler",
         time: "9:00AM",
         kind: "New Patient"
       },
       {
-        num: 3,
+        number: 3,
         name: "Maj. Nikolai Jackov",
         time: "10:00AM",
         kind: "Follow-up"
       },
       {
-        num: 4,
+        number: 4,
         name: "Slater",
         time: "9:30AM",
         kind: "New Patient"
       },
       {
-        num: 5,
+        number: 5,
         name: "Len Drexler",
         time: "10:00AM",
         kind: "New Patient"
--- src/components/AppointmentListComponent.jsx
+++ src/components/AppointmentListComponent.jsx
@@ -4,9 +4,31 @@ const AppointmentList = ({ appointments }) => {
   if (!appointments) {
     return null;
   } else {
+    const rows = appointments.patients.map(patient => {
+      return (
+        <tr key={patient.name}>
+          <td>{patient.number}</td>
+          <td>{patient.name}</td>
+          <td>{patient.time}</td>
+          <td>{patient.kind}</td>
+        </tr>
+      );
+    });
     return (
       <div className="appointment-list-container">
         <div>{`Dr. ${appointments.firstName} ${appointments.lastName}`}</div>
+        <div>{appointments.email}</div>
+        <table className="appointments-table">
+          <tbody>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Time</th>
+              <th>Kind</th>
+            </tr>
+            {rows}
+          </tbody>
+        </table>
       </div>
     );
   }
