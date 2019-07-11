642ac29e47cc4b3be60bf0796e81fd97fbdf61df
Phu <phugnguyen@gmail.com>
2019-07-11 12:14:54 -0700

    basic-react-app

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="calendar-body">
+        <h1>Welcome to React app</h1>
+      </div>
+    );
+  }
+}
